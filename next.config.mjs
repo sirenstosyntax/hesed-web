43cd348d3cb7d7937481faa3ead26e802c6dcdc8
Grant Collings <jacklexidrew@gmail.com>
2026-06-07 12:15:03 -0500

    Add service worker via next-pwa

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,15 @@
+import withPWA from 'next-pwa';
+
+const pwaConfig = withPWA({
+  dest: 'public',
+  register: true,
+  skipWaiting: true,
+  disable: process.env.NODE_ENV === 'development',
+});
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   /* config options here */
 };
 
-export default nextConfig;
+export default pwaConfig(nextConfig);
